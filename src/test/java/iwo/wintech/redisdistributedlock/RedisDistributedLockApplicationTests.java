9d65d352cfb800b229af430591c5be3a8193ed20
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2024-05-10 14:59:12 +0100

    Initial commit

---
+++ src/test/java/iwo/wintech/redisdistributedlock/RedisDistributedLockApplicationTests.java
@@ -0,0 +1,13 @@
+package iwo.wintech.redisdistributedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisDistributedLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
